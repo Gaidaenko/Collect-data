cdfb65c1fb65c354917cd2ad95d55d19d1d6d762
Gaidaenko <45212496+Gaidaenko@users.noreply.github.com>
2022-08-28 21:14:23 +0300

    copy found files

--- Collect data/Program.cs
+++ Collect data/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
-
+using System.IO;
 
 namespace Collect_data
 {
     class Program
-    {  
+    {
+        public static string targetPath = "C:\\tmp";
         static void Main(string[] args)
         {
+            DirectoryInfo targetFolder = new DirectoryInfo("C:\\tmp");
+
+            if (!targetFolder.Exists)
+            {
+                Directory.CreateDirectory("C:\\tmp");
+            }
+
             docx_doc.Docx_doc();
             xlsx_xls.Xlsx_xls();
             pptx.Pptx();
--- Collect data/docx_doc.cs
+++ Collect data/docx_doc.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,14 +8,23 @@ using System.Threading.Tasks;
 namespace Collect_data
 {
     class docx_doc
-    {
+    {    
         public static void Docx_doc()
         {
+            DirectoryInfo targetFolder = new DirectoryInfo("C:\\tmp");
 
             foreach (var items in file_paths.files_docx_desktop)
             {
-                string result = items;
-                Console.WriteLine(result);
+                if (targetFolder.Exists)
+                {
+                    Directory.CreateDirectory("C:\\tmp\\Desktop");
+
+                    string result = Path.GetFileName(items);
+                    string copyFile = Path.Combine("C:\\tmp\\Desktop", result);
+                    File.Copy(items, copyFile, true);
+                }
+
+                Console.WriteLine(items);
             }
 
             foreach (var items in file_paths.files_docx_documets)
